f6f3de0b77791de6ff5b3a0868e968342e7a4841
gregoryv <gregoryv@myumanitoba.ca>
2023-02-03 23:32:56 -0600

    initial sign in screen, still a work in progress

--- app/src/main/java/com/example/orderup/MainActivity.java
+++ app/src/main/java/com/example/orderup/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.orderup;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
+
+    private String email;
+    private String password;
+
+    EditText emailInput;
+    EditText passwordInput;
+
+    Button submitButton;
+
+    private String firstName;
+    private String lastName;
+    private String creditCardNum;
+    private int accountBalance;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailInput = (EditText) findViewById(R.id.emailInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = emailInput.getText().toString();
+                password = passwordInput.getText().toString();
+
+                showToast(email);
+                showToast(password);
+            }
+        });
+
+    }
+
+    private void showToast(String text){
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 }
