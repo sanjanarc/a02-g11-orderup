edeb3a7536a7b087b82faa741df34a02cd738d38
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-02-10 15:09:35 -0600

    Same as sample class.

---
+++ app/src/main/java/com/example/orderup/logic/Services.java
@@ -0,0 +1,18 @@
+package com.example.orderup.logic;
+
+import com.example.orderup.persistance.UserPersistence;
+import com.example.orderup.persistance.stub.UserPersistenceStub;
+
+public class Services {
+
+    private static UserPersistence userPersistence= null;
+
+    public static synchronized UserPersistence getUserPersistence(){
+
+        if(userPersistence== null){
+            userPersistence= new UserPersistenceStub();
+        }
+
+        return userPersistence;
+    }
+}
