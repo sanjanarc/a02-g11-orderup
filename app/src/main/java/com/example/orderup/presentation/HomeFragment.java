34ab78022694dff8cdcb2ae796746cfb1464dd90
Sanjana <roychows@myumanitoba.ca>
2023-02-11 17:19:48 -0600

    home feature added

--- app/src/main/java/com/example/orderup/presentation/HomeFragment.java
+++ app/src/main/java/com/example/orderup/presentation/HomeFragment.java
@@ -1,8 +1,14 @@
 package com.example.orderup.presentation;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.LinearLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.appcompat.widget.SearchView;
 
 import androidx.fragment.app.Fragment;
@@ -16,40 +22,48 @@ import com.example.orderup.logic.Search_algorithm;
 
 public class HomeFragment extends Fragment {
 
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
         //get ids
-        SearchView searchView = findViewById(R.id.searchView);
-        LinearLayout container = findViewById(R.id.textview_container);
+        SearchView searchView = rootView.findViewById(R.id.searchView);
+        LinearLayout containerText = rootView.findViewById(R.id.textview_container);
+
+
+
 
         //search bar
         searchView.setQuery("Search Restaurants and Cuisines..",false);
-        searchView.setOnQueryTextListener(new searchView.OnQueryTextListener() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                ArrayList<String> results = Search_algorithm.searchRestaurant(query);
-                return true;
+                ArrayList<String> results = new ArrayList<>();
+                results = Search_algorithm.searchRestaurant(query);
 
                 if(results.isEmpty()){
                     String msg = "Your search does not match any restaurants on OrderUp";
-                    TextView textView = new TextView(this);
+                    TextView textView = new TextView(getActivity());
                     textView.setText(msg);
                     textView.setTextSize(12);
-                    container.addView(textView);
+                    containerText.addView(textView);
                 }
                 else {
                     for (int i = 0; i < results.size(); i++) {
-                        TextView textView = new TextView(this);
+                        TextView textView = new TextView(getActivity());
                         textView.setText(results.get(i));
                         textView.setTextSize(18);
-                        textView.setTextColor(Color.PURPLE);
-                        container.addView(textView);
+                        //textView.setTextColor(Color.PURPLE);
+                        containerText.addView(textView);
                     }
 
                 }
+                return true;
 
             }
 
@@ -59,7 +73,11 @@ public class HomeFragment extends Fragment {
                 return true;
             }
         });
+
+        // Inflate the layout for this fragment
+        return view;
     }
+
 
 
 
