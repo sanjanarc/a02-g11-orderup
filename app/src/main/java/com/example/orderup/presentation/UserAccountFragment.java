d897f605b9b02b8e17e3b44bf833c7e6d69a9511
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-02-10 19:04:12 -0600

    Add credit card and add address Done.

--- app/src/main/java/com/example/orderup/presentation/UserAccountFragment.java
+++ app/src/main/java/com/example/orderup/presentation/UserAccountFragment.java
@@ -1,5 +1,7 @@
 package com.example.orderup.presentation;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -9,15 +11,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.orderup.R;
 import com.example.orderup.logic.Services;
+import com.example.orderup.persistance.UserPersistence;
 
 public class UserAccountFragment extends Fragment {
 
     TextView infoContainer;
-    Button addCardButton, logoutButton, addAddress;
+    Button addCardButton, logoutButton, addAddressButton;
+
+    UserPersistence userPersistence= Services.getUserPersistence();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,22 +31,22 @@ public class UserAccountFragment extends Fragment {
         View view= inflater.inflate(R.layout.fragment_user_account, container, false);
 
         infoContainer= (TextView) view.findViewById(R.id.infoContainer);
-        infoContainer.setText(Services.getUserPersistence().getUserList().get(getActivity().getIntent().getStringExtra("email")).toString());
+        infoContainer.setText(userPersistence.getUserList().get(getActivity().getIntent().getStringExtra("email")).toString());
         infoContainer.setTextSize(30);
 
         addCardButton= (Button) view.findViewById(R.id.addCardButton);
         addCardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Pop up a dialog to add credit card.
+                addCardPopUp();
             }
         });
 
-        addAddress= (Button) view.findViewById(R.id.addAddressButton);
-        addAddress.setOnClickListener(new View.OnClickListener() {
+        addAddressButton= (Button) view.findViewById(R.id.addAddressButton);
+        addAddressButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Pop up a dialog to add address.
+                addAddressPopUp();
             }
         });
 
@@ -55,5 +61,47 @@ public class UserAccountFragment extends Fragment {
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+    private void addCardPopUp(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Enter your Credit Card Info:");
+        View v = getLayoutInflater().inflate(R.layout.popup_add_credit_card, null);
+        builder.setView(v);
+        builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                EditText cardNumInput= (EditText) v.findViewById(R.id.cardNumberInput);
+                EditText cardCvcInput= (EditText) v.findViewById(R.id.cardCvcInput);
+                EditText cardExpiryInput= (EditText) v.findViewById(R.id.cardExpiryInput);
+
+                int cardNum= Integer.parseInt(cardNumInput.getText().toString());
+                int cardCvc= Integer.parseInt(cardCvcInput.getText().toString());
+                String cardExpiry= cardExpiryInput.getText().toString();
+
+                userPersistence.addCreditCard(getActivity().getIntent().getStringExtra("email"), cardNum, cardCvc, cardExpiry);
+            }
+        });
+
+        builder.show();
+    }
+
+    private void addAddressPopUp(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Enter your Address:");
+        View v = getLayoutInflater().inflate(R.layout.popup_add_address, null);
+        builder.setView(v);
+        builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                EditText addressInput= (EditText) v.findViewById(R.id.addressInput);
+
+                String address= addressInput.getText().toString();
+
+                userPersistence.updateAddress(getActivity().getIntent().getStringExtra("email"), address);
+            }
+        });
+
+        builder.show();
     }
 }
