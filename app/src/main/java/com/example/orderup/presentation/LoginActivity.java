08773c196d83efbec2d69636d99ae108d7067690
gregoryv <gregoryv@myumanitoba.ca>
2023-02-23 22:04:06 -0600

    Finish adding button to display the user database contents for development purposes

--- app/src/main/java/com/example/orderup/presentation/LoginActivity.java
+++ app/src/main/java/com/example/orderup/presentation/LoginActivity.java
@@ -1,12 +1,14 @@
 package com.example.orderup.presentation;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.orderup.R;
@@ -91,6 +93,34 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    public void showMessage(String title, String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
 
+    public void printDatabase() {
+        // Print database contents
+        Cursor res = myDatabase.getAllData();
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext()) {
+            buffer.append("Id :" + res.getString(0) + "\n");
+            buffer.append("Email :" + res.getString(1) + "\n");
+            buffer.append("Password :" + res.getString(2) + "\n");
+            buffer.append("First Name :" + res.getString(3) + "\n");
+            buffer.append("Last Name :" + res.getString(4) + "\n");
+            buffer.append("Credit Card :" + res.getString(5) + "\n");
+            buffer.append("CSV :" + res.getString(6) + "\n");
+            buffer.append("Expiry :" + res.getString(7) + "\n");
+            buffer.append("Address :" + res.getString(8) + "\n");
+            buffer.append("Account Balance :$" + res.getString(9) + "\n\n");
+        }
+
+        //Show all data
+        showMessage("Data",buffer.toString());
+
+    }
 
 }
