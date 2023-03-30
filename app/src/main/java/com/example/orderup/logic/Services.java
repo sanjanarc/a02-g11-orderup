2668e783165b24300f2b47758caf230d2b95c179
gregoryv <gregoryv@myumanitoba.ca>
2023-03-29 21:37:57 -0500

    Remove Stubs

--- app/src/main/java/com/example/orderup/logic/Services.java
+++ app/src/main/java/com/example/orderup/logic/Services.java
@@ -1,13 +1,9 @@
 package com.example.orderup.logic;
 
-import android.util.Log;
-
 import com.example.orderup.persistance.RestaurantPersistence;
 import com.example.orderup.persistance.UserPersistence;
 import com.example.orderup.persistance.hsqldb.RestaurantPersistenceHSQLDB;
 import com.example.orderup.persistance.hsqldb.UserPersistenceHSQLDB;
-import com.example.orderup.persistance.stub.RestaurantPersistenceStub;
-import com.example.orderup.persistance.stub.UserPersistenceStub;
 
 //Class that holds most the static variables.
 public class Services
