27a48edbb18beffa065603e58defb2b089e833fb
Kru-pal <118309054+Kru-pal@users.noreply.github.com>
2023-04-04 13:53:01 -0500

    the cart doesnt work yet(SOS).

--- app/src/main/java/com/example/orderup/presentation/myCartAdapter.java
+++ app/src/main/java/com/example/orderup/presentation/myCartAdapter.java
@@ -1,5 +1,6 @@
 package com.example.orderup.presentation;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,37 +15,49 @@ import com.example.orderup.R;
 import java.util.List;
 
 //This class is the structure of all the adapter.
-public class myCartAdapter extends RecyclerView.Adapter<MenuHolder> {
+public class myCartAdapter extends RecyclerView.Adapter<myCartHolder> {
 
+    Context context;
     List<FoodItem> foods;
 
-    myCartAdapter(List<FoodItem> foods)
+    myCartAdapter(Context context,List<FoodItem> foods)
     {
+        this.context = context;
         this.foods = foods;
+        Log.d("myadapterconstructor:     ", this.foods.get(0).getItemDescription());
     }
+
     @NonNull
     @Override
-    public MenuHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.mycartadapter, parent, false);
-
-        return new MenuHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.mycartadapter,parent,false));
+    public myCartHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new myCartHolder(LayoutInflater.from(context).inflate(R.layout.mycartadapter,parent,false));
     }
+
     @Override
-    public void onBindViewHolder(@NonNull MenuHolder holder, int position) {
+    public void onBindViewHolder(@NonNull myCartHolder holder, int position) {
 
-        FoodItem foodItem = foods.get(position);
-        String foodName = foodItem.getItemName();
-        String foodDes = foodItem.getItemDescription();
-        String foodPrice = String.valueOf(foodItem.getItemPrice());
-        String info = foodName+"\n"+foodDes+"\nPrice: "+foodPrice;
-        holder.nameViewCart.setText(info);
-        int url = holder.imageViewCart.getResources().getIdentifier(foodItem.getImageUrl(), "drawable", MainActivity.PACKAGE_NAME);
-        holder.imageViewCart.setBackgroundResource(url);
-        Log.d("The myCartAdapter class:::::::::", foodName);
-
+        holder.textView.setText(foods.get(position).getItemName());
+        holder.imageView.setImageResource(foods.get(position).getItem_id());
+        int url = holder.imageView.getResources().getIdentifier(foods.get(position).getImageUrl(), "drawable", MainActivity.PACKAGE_NAME);
+        holder.imageView.setBackgroundResource(url);
 
     }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull MenuHolder holder, int position) {
+//
+//        FoodItem foodItem = foods.get(position);
+//        String foodName = foodItem.getItemName();
+//        String foodDes = foodItem.getItemDescription();
+//        String foodPrice = String.valueOf(foodItem.getItemPrice());
+//        String info = foodName+"\n"+foodDes+"\nPrice: "+foodPrice;
+//        holder.nameViewCart.setText(info);
+//        int url = holder.imageViewCart.getResources().getIdentifier(foodItem.getImageUrl(), "drawable", MainActivity.PACKAGE_NAME);
+//        holder.imageViewCart.setBackgroundResource(url);
+//        Log.d("The myCartAdapter class:::::::::", foodName);
+//
+//
+//    }
 
     @Override
     public int getItemCount() {
