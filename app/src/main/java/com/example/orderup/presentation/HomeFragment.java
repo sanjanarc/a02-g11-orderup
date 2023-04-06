ea1ba0afd264f56abb6b6f66f52490653fb8316d
Sanjana <roychows@myumanitoba.ca>
2023-04-05 22:01:27 -0500

    search feature is now working

--- app/src/main/java/com/example/orderup/presentation/HomeFragment.java
+++ app/src/main/java/com/example/orderup/presentation/HomeFragment.java
@@ -13,11 +13,13 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.orderup.Objects.Restaurant;
 import com.example.orderup.R;
 import com.example.orderup.logic.RestaurantServices;
 import com.example.orderup.logic.Search_algorithm;
 
 import com.example.orderup.logic.Services;
+import java.util.List;
 
 /**
  * This is the home page UI class.
@@ -52,8 +54,9 @@ public class HomeFragment extends Fragment {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                Log.d("user input", s);
+                //Log.d("user input", s);
                 String inputText = s;
+                List<Restaurant>  newRestaurantList = Search_algorithm.searchRestaurant(inputText);
                 recyclerView.setAdapter(new RestaurantAdapter(Search_algorithm.searchRestaurant(inputText)));
                 return false;
             }
