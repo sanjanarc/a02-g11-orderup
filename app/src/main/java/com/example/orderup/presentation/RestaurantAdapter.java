f665483f158b84fb87915ef025afd4a9e2a633e3
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-04-08 05:35:18 +0800

    Organizing pause and start merging with search algorithm.

--- app/src/main/java/com/example/orderup/presentation/RestaurantAdapter.java
+++ app/src/main/java/com/example/orderup/presentation/RestaurantAdapter.java
@@ -11,30 +11,30 @@ import com.example.orderup.Objects.Restaurant;
 
 import java.util.List;
 
-//This class set the restaurant list align and show on the home fragment.
-public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantHolder>
-{
+/**
+ * This class set the restaurant list align and show on the home fragment.
+ */
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantHolder> {
+
     List<Restaurant> restaurants;
-    RestaurantAdapter(List<Restaurant> restaurants)
-    {
+
+    RestaurantAdapter(List<Restaurant> restaurants) {
         this.restaurants = restaurants;
     }
 
     @NonNull
     @Override
-    public RestaurantHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
-    {
-        return new RestaurantHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_view,parent,false));
+    public RestaurantHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new RestaurantHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_view, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RestaurantHolder holder, int position)
-    {
-        holder.nameview.setText(restaurants.get(position).getRestaurantName());
-        holder.descriptionview.setText(restaurants.get(position).getRestaurantDescription());
-        int url = holder.imageview.getResources().getIdentifier(restaurants.get(position).getImagesURL(), "drawable", MainActivity.PACKAGE_NAME);
-        holder.imageview.setBackgroundResource(url);
-        holder.position = holder.getAdapterPosition(); //Pass current position back to super class and knowing that which restaurant get clicked.
+    public void onBindViewHolder(@NonNull RestaurantHolder holder, int position) {
+        holder.nameview.setText(restaurants.get(position).getRestaurantName()); // Get the restaurant name.
+        holder.descriptionview.setText(restaurants.get(position).getRestaurantDescription()); // Get restaurant description.
+        int url = holder.imageview.getResources().getIdentifier(restaurants.get(position).getImagesURL(), "drawable", MainActivity.PACKAGE_NAME); // Get image url.
+        holder.imageview.setBackgroundResource(url); // Set the restaurant background image.
+        holder.position = holder.getAdapterPosition(); // Pass current position back to super class and knowing that which restaurant get clicked.
     }
 
     @Override
