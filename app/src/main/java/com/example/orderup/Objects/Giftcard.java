286c02fe474ff7bd578535d346c43fca4f688cea
gregoryv <gregoryv@myumanitoba.ca>
2023-04-07 19:14:35 -0500

    Add comments to Giftcard.java

--- app/src/main/java/com/example/orderup/Objects/Giftcard.java
+++ app/src/main/java/com/example/orderup/Objects/Giftcard.java
@@ -4,9 +4,12 @@ public class Giftcard {
     String number;
     float amount;
 
-
-
-
+    /**
+     * Constructor.
+     *
+     * @param number the gift card 16 digit numeric code.
+     * @param amount the dollar amount on the giftcard.
+     */
     public Giftcard(String number, float amount) {
         this.number = number;
         this.amount = amount;
