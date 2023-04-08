a3c6e97fb2a6472d50e0e08eeebc8e7e9f760c67
gregoryv <gregoryv@myumanitoba.ca>
2023-04-07 21:57:43 -0500

    Add comments to RestaurantPersistence.java

--- app/src/main/java/com/example/orderup/persistance/RestaurantPersistence.java
+++ app/src/main/java/com/example/orderup/persistance/RestaurantPersistence.java
@@ -9,14 +9,35 @@ import java.util.List;
  */
 public interface RestaurantPersistence {
 
-    //Get a list of restaurant from database.
+    /**
+     * Get a list of restaurant from database.
+     *
+     * @return a list of Restaurant objects.
+     */
     List<Restaurant> getRestaurantSequential();
 
+    /**
+     * Get restaurant object by id.
+     *
+     * @param id the user id.
+     * @return a Restaurant object.
+     */
     Restaurant getRest(int id);
 
     //Insert new user comment to database.
+    /**
+     * Insert new user comment to database.
+     *
+     * @param restID the restaurant id.
+     * @param comment the comment string.
+     */
     void insertComment(int restID, String comment);
 
-    //Get the newest user comments.
+    /**
+     * Get the newest user comments.
+     *
+     * @param id the restaurant id.
+     * @return a list of comment strings.
+     */
     List<String> getComments(int id);
 }
