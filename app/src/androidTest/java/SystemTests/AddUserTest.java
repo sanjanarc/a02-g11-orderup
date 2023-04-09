74de3a0423cb33e03df04fa5e6878e2446c63538
Harsimran <dhillo42@myumanitoba.ca>
2023-04-08 22:44:43 -0500

    Systemtest folder created + app's build.gradle updated for espresso

---
+++ app/src/androidTest/java/SystemTests/AddUserTest.java
@@ -0,0 +1,32 @@
+package SystemTests;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.anything;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+/*
+        Purpose: This class tests Registering a User's account
+ */
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class AddUserTest {
+
+
+}
