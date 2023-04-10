05f9c67f2b5b26b7495cd95c903efe2ba5f7e992
Harsimran <dhillo42@myumanitoba.ca>
2023-04-09 22:16:08 -0500

    AddCommentTest.java-> COMPLETE

--- app/src/androidTest/java/SystemTests/AddCommentTest.java
+++ app/src/androidTest/java/SystemTests/AddCommentTest.java
@@ -1,15 +1,15 @@
 package SystemTests;
+
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.StringContains.containsString;
 
-import static org.hamcrest.CoreMatchers.anything;
-
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -21,7 +21,6 @@ import org.junit.runner.RunWith;
 
 import com.example.orderup.R;
 import com.example.orderup.presentation.LoginActivity;
-import com.example.orderup.presentation.RestaurantActivity;
 
 /*
     Purpose: Test adding a comment in a restaurant's comment section
@@ -32,18 +31,26 @@ public class AddCommentTest {
 
     @Rule
     public ActivityScenarioRule<LoginActivity> loginActivity = new ActivityScenarioRule<LoginActivity>(LoginActivity.class);
-    //public ActivityScenarioRule<RestaurantActivity> restaurantActivity = new ActivityScenarioRule<RestaurantActivity>(RestaurantActivity.class);
-
+    @Test
     public void testComment(){
 
         //login
         onView(withId(R.id.emailInput)).perform(typeText("admin2@email.com"));
         onView(withId(R.id.passwordInput)).perform(typeText("admin123"));
-
-        //click on sign in
         onView(withId(R.id.signInButton)).perform(click());
-       // onView(withId(R.id.))
 
+        //click on the first restaurant in the list on the home page
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        //leave a comment
+        onView(withId(R.id.comment_Button)).perform(click());
+        onView(withId(R.id.inputComment)).perform(typeText("10/10"));
+        onView(withId(R.id.sendComment)).perform(click());
+        //check the comment was left in the comment section
+        pressBack();
+        pressBack();
+        onView(withId(R.id.recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.comment_Button)).perform(click());
+        onView(withId(R.id.commentSection)).check(matches(withText(containsString("10/10"))));
     }
 
 }
