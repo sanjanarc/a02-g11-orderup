3502621b3c76fe58e6ea78f8e6e6a7608b2524b6
Harsimran <dhillo42@myumanitoba.ca>
2023-04-10 11:46:50 -0500

    AddCreditCard-> COMPLETE.

--- app/src/androidTest/java/AllAcceptanceTests.java
+++ app/src/androidTest/java/AllAcceptanceTests.java
@@ -3,7 +3,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import SystemTests.AddCommentTest;
+import SystemTests.AddCreditCard;
+import SystemTests.AddUserAddress;
 import SystemTests.AddUserTest;
+import SystemTests.AddingToCartTest;
 import SystemTests.LoginTest;
 import SystemTests.SearchRestaurantTest;
 import SystemTests.BuyMembershipTest;
@@ -19,7 +22,10 @@ import SystemTests.BuyMembershipTest;
         AddUserTest.class,
         SearchRestaurantTest.class,
         AddCommentTest.class,
-        BuyMembershipTest.class
+        BuyMembershipTest.class,
+        AddCreditCard.class,
+        AddUserAddress.class,
+        AddingToCartTest.class
 
 })
 public class AllAcceptanceTests {
